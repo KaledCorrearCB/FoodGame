a0c2abf197923b0f2bef53533117bdaccfa6247e
KaledCorrearCB <56941264+KaledCorrearCB@users.noreply.github.com>
2025-10-19 14:08:45 -0500

    PersonajesPuestos
    PersonajesFueronPuestos

--- Doom Basic/Doom Basic/Assets/_Scripts/Menus/MainMenu.cs
+++ Doom Basic/Doom Basic/Assets/_Scripts/Menus/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("Enemy and Shooting");
     }
 
     public void QuitGame()
